6905f6ab903bdfc88624926542ba284e7971c8ad
Borja Cabello Luna <borjacabello@outlook.es>
2022-09-27 22:38:21 +0200

    falta exercise 4

--- js/index.js
+++ js/index.js
@@ -43,27 +43,93 @@ function renderPepperoni() {
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach( (eachMushroom) => {
+    if (state.mushrooms) {
+      eachMushroom.style.visibility = 'visible';
+    } else {
+      eachMushroom.style.visibility = 'hidden';
+    }
+  })
 }
 
 function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach( (eachPepper) => {
+    if (state.greenPeppers) {
+      eachPepper.style.visibility = "visible";
+    } else {
+      eachPepper.style.visibility = "hidden";
+    }
+  })
 }
 
 function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  let whiteSauceDOM = document.querySelector(".sauce");
+
+  if (state.whiteSauce) {
+    whiteSauceDOM.classList.add("sauce-white");
+  } else {
+    whiteSauceDOM.classList.remove("sauce-white");
+  }
+
 }
 
 function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  let crustDOM = document.querySelector(".crust");
+
+  if (state.glutenFreeCrust) {
+    crustDOM.classList.add("crust-gluten-free");
+  } else {
+    crustDOM.classList.remove("crust-gluten-free");
+  }
+
 }
 
+ // Iteration 3: add/remove the class "active" of each `<button class="btn">`
 function renderButtons() {
-  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  let pepeperoniButton = document.querySelector('.btn-pepperoni')
+  let mushroomButton = document.querySelector('.btn-mushrooms')
+  let pepperButton = document.querySelector('.btn-green-peppers')
+  let sauceButton = document.querySelector('.btn-sauce')
+  let crustButton = document.querySelector('.btn-crust')
+
+  if (state.pepperoni) {
+    pepeperoniButton.classList.add("active")
+  } else {
+    pepeperoniButton.classList.remove("active")
+  }
+
+  if (state.mushrooms) {
+    mushroomButton.classList.add("active")
+  } else {
+    mushroomButton.classList.remove("active")
+  }
+
+  if (state.greenPeppers) {
+    pepperButton.classList.add("active")
+  } else {
+    pepperButton.classList.remove("active")
+  }
+
+  if (state.whiteSauce) {
+    sauceButton.classList.add("active")
+  } else {
+    sauceButton.classList.remove("active")
+  }
+
+  if (state.crust) {
+    crustButton.classList.add("active")
+  } else {
+    crustButton.classList.remove("active")
+  }
 }
 
 function renderPrice() {
-  // Iteration 4: change the HTML of `<aside class="panel price">`
+  let panelDOM = document.querySelector(".panel ul");
+  panelDOM.innerHTML = "";
+
+  // creo que haría un bucle que en función de state añadiese elementos html pero no acabo de verlo
+
+  //console.log(panelDOM.innerHTML)
 }
 
 renderEverything();
@@ -75,9 +141,23 @@ document.querySelector('.btn.btn-pepperoni').addEventListener('click', function 
 });
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-
+document.querySelector(".btn.btn-mushrooms").addEventListener('click', () => {
+  state.mushrooms = !state.mushrooms;
+  renderEverything();
+})
 // Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-
+document.querySelector(".btn.btn-green-peppers").addEventListener('click', () => {
+  state.greenPeppers = !state.greenPeppers;
+  renderEverything();
+})
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector(".btn.btn-sauce").addEventListener('click', () => {
+  state.whiteSauce = !state.whiteSauce;
+  renderEverything();
+})
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector(".btn.btn-crust").addEventListener('click', () => {
+  state.glutenFreeCrust = !state.glutenFreeCrust;
+  renderEverything();
+})
